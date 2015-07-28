4853ca81756a4698fa3b2692ad5890e504e8c1d3
dc <dc@as>
2015-07-28 16:56:11 +0800

    implicit intents

--- app/src/main/java/com/example/ds/sunshine/ForecastFragment.java
+++ app/src/main/java/com/example/ds/sunshine/ForecastFragment.java
@@ -218,15 +218,13 @@ public class ForecastFragment extends Fragment {
         /**
          * Prepare the weather high/lows for presentation.
          */
-        private String formatHighLows(double high, double low) {
+        private String formatHighLows(double high, double low, String unitType) {
             // For presentation, assume the user doesn't care about tenths of a degree.
-            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
-            String unitType = pref.getString(
-                    getString(R.string.pref_units_key),
-                    getString(R.string.pref_units_metric)
-            );
+
             if(unitType.equals(getString(R.string.pref_units_imperial))){
                 //conversion
+                high = (high * 1.8) + 32;
+                low = (low * 1.8) + 32;
             }else if(!unitType.equals(getString(R.string.pref_units_metric))){
                 Log.d(LOG_TAG, "Unit type not found: " + unitType);
             }
@@ -276,6 +274,11 @@ public class ForecastFragment extends Fragment {
             dayTime = new Time();
 
             String[] resultStrs = new String[numDays];
+            SharedPreferences pref = PreferenceManager.getDefaultSharedPreferences(getActivity());
+            String unitType = pref.getString(
+                    getString(R.string.pref_units_key),
+                    getString(R.string.pref_units_metric)
+            );
             for(int i = 0; i < weatherArray.length(); i++) {
                 // For now, using the format "Day, description, hi/low"
                 String day;
@@ -303,7 +306,7 @@ public class ForecastFragment extends Fragment {
                 double high = temperatureObject.getDouble(OWM_MAX);
                 double low = temperatureObject.getDouble(OWM_MIN);
 
-                highAndLow = formatHighLows(high, low);
+                highAndLow = formatHighLows(high, low, unitType);
                 resultStrs[i] = day + " - " + description + " - " + highAndLow;
             }
             return resultStrs;
